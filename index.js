af8c9fd66cd2f9c544ed28373ab6d1edb2b497e0
Andrija Petrovic <andrija.hers@gmail.com>
2017-08-17 07:56:28 -0400

    Supporting `npm prefix` dir structure on Windows

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var Path = require('path'),
   Child_process = require('child_process'),
   commandExistsSync = require('command-exists').sync,
+  Os = require('os'),
+  _isNotWin = Os.type() !== 'Windows_NT',
   _root = null;
 
 function configroot () {
   if (_root === null) {
     if (commandExistsSync('npm')) {
-      _root = Path.resolve(Child_process.execSync('npm config get prefix', {}).toString().trim(), 'lib');
+      _root = Child_process.execSync('npm config get prefix', {}).toString().trim();
+      if (_isNotWin) {
+        _root = Path.resolve(_root, 'lib');
+      }
     } else {
       _root = process.cwd();
     }
